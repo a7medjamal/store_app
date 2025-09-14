e836f291a292860f41b4915d8890a6c150debeeb
a7medjamal <ahmedjmal20003@gmail.com>
2025-09-14 22:21:31 +0300

    feat(splash): add splash screen assets and configuration
    - Add splash screen images for both Android and iOS platforms
    - Configure flutter_native_splash package with brand color and image
    - Update Android styles and launch background for splash screen
    - Modify iOS LaunchScreen.storyboard and Info.plist for splash screen
    - Remove unused home view files and clean up routing

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,10 +1,7 @@
 import 'package:go_router/go_router.dart';
-import 'package:store_app/features/home/presentation/views/home_view.dart';
 
 abstract class AppRouter {
-  static final GoRouter router = GoRouter(
-    routes: [
-      GoRoute(path: '/', builder: (context, state) => const HomeView()),
+  static final GoRouter router = GoRouter(routes: [
     ],
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:store_app/core/utils/app_router.dart';
 
+void main() {
+  runApp(const StoreApp());
+}
+
 class StoreApp extends StatelessWidget {
   const StoreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       routerConfig: AppRouter.router,
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
     );
