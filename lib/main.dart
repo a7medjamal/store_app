cc95eb7669e7f06dab9cad4147b9e239cade9209
a7medjamal <ahmedjmal20003@gmail.com>
2025-09-21 13:43:22 +0300

    feat(firebase): add firebase core integration and env support
    - Add firebase_core and flutter_dotenv dependencies
    - Configure firebase for Android and IOS
    - Generate firebase options from environment variables
    - Update .gitignore to exclude sensitive configuration files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:store_app/core/utils/app_router.dart';
 import 'package:store_app/core/utils/app_colors.dart';
+import 'package:store_app/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const StoreApp());
 }
 
